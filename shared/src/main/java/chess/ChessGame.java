5ca88517545a894cd65a7b8b721bfa5e123d5ecc
apretado <jithorn+github@byu.edu>
2025-02-05 17:48:59 -0700

    Implement isInStalemate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -194,7 +194,7 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return !isInCheck(teamColor) && !hasValidMoves(teamColor);
     }
 
     /**
