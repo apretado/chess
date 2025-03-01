d0ba36b2b144a9ddfb12db3c3368fa0ea306ba03
apretado <156238182+apretado@users.noreply.github.com>
2025-02-28 19:45:22 -0700

    Add rename methods to GameData

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,5 +1,10 @@
 package model;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, String game) {
-
+    GameData renameWhite(String newWhiteUsername) {
+        return new GameData(gameID, newWhiteUsername, blackUsername, gameName, game);
+    }
+    GameData renameBlack(String newBlackUsername) {
+        return new GameData(gameID, whiteUsername, newBlackUsername, gameName, game);
+    }
 }
