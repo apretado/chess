ee8af5026ab49a8633394ead1a8b9fb69c666380
apretado <156238182+apretado@users.noreply.github.com>
2025-03-12 16:19:25 -0600

    Implement createUser metod

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -1,6 +1,9 @@
 package dataaccess;
 
 import java.sql.SQLException;
+import java.sql.DriverManager;
+
+import com.google.gson.Gson;
 
 import model.UserData;
 
@@ -12,8 +15,20 @@ public class MySqlUserDAO implements UserDAO {
 
     @Override
     public void createUser(UserData userData) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+        try (var conn = DatabaseManager.getConnection()) {
+            // Try to add user to database
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO UserData (username, password, email) VALUES (?, ?, ?)")) {
+                preparedStatement.setString(1, userData.username());
+                preparedStatement.setString(2, userData.email());
+                preparedStatement.setString(3, userData.password());
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            if (e.getErrorCode() == 1062) {
+                throw new DataAccessException("Error: already taken");
+            }
+            throw new DataAccessException("Database error: " + e.getMessage());
+        }
     }
 
     @Override
@@ -42,7 +57,7 @@ public class MySqlUserDAO implements UserDAO {
     private void configureDatabase() throws DataAccessException {
         DatabaseManager.createDatabase();
         try (var conn = DatabaseManager.getConnection()) {
-            for (var statement : createStatements) {
+            for (String statement : createStatements) {
                 try (var preparedStatement = conn.prepareStatement(statement)) {
                     preparedStatement.executeUpdate();
                 }
