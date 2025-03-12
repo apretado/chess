923006c1b5d04c86c124806a52d8d4b863cf5b5a
apretado <156238182+apretado@users.noreply.github.com>
2025-03-12 14:48:01 -0600

    Add configureDatabase method to MySqlUserDAO

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -1,8 +1,14 @@
 package dataaccess;
 
+import java.sql.SQLException;
+
 import model.UserData;
 
 public class MySqlUserDAO implements UserDAO {
+
+    public MySqlUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
 
     @Override
     public void createUser(UserData userData) throws DataAccessException {
@@ -20,6 +26,30 @@ public class MySqlUserDAO implements UserDAO {
     public void clearUser() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'clearUser'");
+    }
+
+    private final String[] createStatements = {
+        """
+        CREATE TABLE IF NOT EXISTS UserData (
+            `username` varchar(256) NOT NULL,
+            `password` varchar(256) NOT NULL,
+            `email` varchar(256) NOT NULL
+            PRIMARY KEY (`username`)
+        )
+        """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: unable to configure database: " + e.getMessage());
+        }
     }
 
 }
