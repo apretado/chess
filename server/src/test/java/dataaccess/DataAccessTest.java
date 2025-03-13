bceb309abf24ee6a4c0fe69b6845aa8d0f718625
apretado <156238182+apretado@users.noreply.github.com>
2025-03-12 19:16:15 -0600

    Create authDAO tests

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -7,18 +7,22 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.AuthData;
 import model.UserData;
 
 public class DataAccessTest {
     UserDAO userDAO;
+    AuthDAO authDAO;
 
     DataAccessTest() throws DataAccessException {
         userDAO = new MySqlUserDAO();
+        authDAO = new MySqlAuthDAO();
     }
 
     @BeforeEach
     void clear() throws DataAccessException {
         userDAO.clearUser();
+        authDAO.clearAuth();
     }
 
     @Test
@@ -85,5 +89,92 @@ public class DataAccessTest {
         assertEquals("Error: unauthorized", exception.getMessage());
     }
 
+    @Test
+    void createAuthSuccess() {
+        // Add auth data
+        assertDoesNotThrow(() -> authDAO.createAuth(new AuthData("authToken", "username")));
+    }
+
+    @Test
+    void creatAuthFail() {
+        // Add auth data
+        AuthData authData = new AuthData("authToken", "username");
+        assertDoesNotThrow(() -> authDAO.createAuth(authData));
+
+        // Try to add it again
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            authDAO.createAuth(authData);
+        });
+    }
+
+    @Test
+    void getAuthSuccess() {
+        // Add auth data
+        AuthData inputAuthData = new AuthData("authToken", "username");
+        assertDoesNotThrow(() -> authDAO.createAuth(inputAuthData));
+
+        // Get auth data
+        AuthData outpAuthData = assertDoesNotThrow(() -> authDAO.getAuth("authToken"));
+        assertEquals(inputAuthData, outpAuthData);
+    }
+
+    @Test
+    void getAuthFail() {
+        // Try to get non existent auth data
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            authDAO.getAuth("invalidAuthToken");
+        });
+        assertEquals("Error: unauthorized", exception.getMessage());
+    }
+
+    @Test
+    void deleteAuthSuccess() {
+        // Add auth data
+        AuthData inputAuthData = new AuthData("authToken", "username");
+        assertDoesNotThrow(() -> authDAO.createAuth(inputAuthData));
+
+        // Delete auth data
+        assertDoesNotThrow(() -> authDAO.deleteAuth("authToken"));
+
+        // Check if it was deleted
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            authDAO.getAuth("authToken");
+        });
+        assertEquals("Error: unauthorized", exception.getMessage());
+    }
+
+    @Test
+    void deleteAuthFail() {
+        // Add auth data
+        AuthData inputAuthData = new AuthData("authToken", "username");
+        assertDoesNotThrow(() -> authDAO.createAuth(inputAuthData));
+
+        // Try to delete auth data with wrong token
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            authDAO.getAuth("badToken");
+        });
+        assertEquals("Error: unauthorized", exception.getMessage());
+    }
+
+    @Test
+    void clearAuthSuccess() throws DataAccessException {
+        // Add some auth data
+        authDAO.createAuth(new AuthData("authToken", "username"));
+        authDAO.createAuth(new AuthData("authToken2", "username2"));
+
+        // Clear the auth data
+        assertDoesNotThrow(() -> authDAO.clearAuth());
+
+        // Check if it was cleared
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            authDAO.getAuth("authToken");
+        });
+        assertEquals("Error: unauthorized", exception.getMessage());
+
+        exception = assertThrows(DataAccessException.class, () -> {
+            authDAO.getAuth("authToken2");
+        });
+        assertEquals("Error: unauthorized", exception.getMessage());
+    }
 
 }
