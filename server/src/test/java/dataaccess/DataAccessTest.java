51dac9f31c77b7e696c7f5b12ec28dbbb33603bf
apretado <156238182+apretado@users.noreply.github.com>
2025-03-12 22:58:01 -0600

    Create GameDAO tests

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -4,25 +4,34 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import chess.ChessGame;
 import model.AuthData;
+import model.GameData;
 import model.UserData;
 
 public class DataAccessTest {
     UserDAO userDAO;
     AuthDAO authDAO;
+    GameDAO gameDAO;
 
     DataAccessTest() throws DataAccessException {
         userDAO = new MySqlUserDAO();
         authDAO = new MySqlAuthDAO();
+        gameDAO = new MySqlGameDAO();
     }
 
     @BeforeEach
     void clear() throws DataAccessException {
         userDAO.clearUser();
         authDAO.clearAuth();
+        gameDAO.clearGame();
     }
 
     @Test
@@ -177,4 +186,129 @@ public class DataAccessTest {
         assertEquals("Error: unauthorized", exception.getMessage());
     }
 
+    @Test
+    void createGameSuccess() throws DataAccessException {
+        // Create game
+        gameDAO.createGame(new GameData(
+                0, null, null, "gameName", new ChessGame()
+        ));
+
+        // Check if it was created
+        GameData expectedGameData = new GameData(1, null, null, "gameName", new ChessGame());
+        GameData actualGameData = gameDAO.getGame(1);
+        assertEquals(expectedGameData, actualGameData);
+    }
+
+    @Test
+    void createGameFail() {
+        // Create game without name
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            gameDAO.createGame(new GameData(
+                0, null, null, null, null
+            ));
+        });
+        assertEquals("Error: game name cannot be null", exception.getMessage());
+    }
+
+    @Test
+    void getGameSuccess() {
+        // 
+    }
+
+    @Test
+    void getGameFail() throws DataAccessException {
+    // Create game
+    gameDAO.createGame(new GameData(
+                0, null, null, "gameName", new ChessGame()
+    ));
+
+    // Try to get game with invalid id
+    DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+        gameDAO.getGame(2);
+    });
+    assertEquals("Error: bad request", exception.getMessage());
+    }
+
+    @Test
+    void listGamesSuccess() throws DataAccessException {
+        // Create games
+        gameDAO.createGame(new GameData(
+            0, null, null, "gameName", new ChessGame()
+        ));
+        gameDAO.createGame(new GameData(
+            0, null, null, "gameName2", new ChessGame()
+        ));
+
+        // List games
+        List<GameData> expectedGameList = new ArrayList<>();
+        expectedGameList.add(new GameData(1, null, null, "gameName", new ChessGame()));
+        expectedGameList.add(new GameData(2, null, null, "gameName2", new ChessGame()));
+        List<GameData> actualGameList = gameDAO.listGames();
+        assertEquals(expectedGameList, actualGameList);
+    }
+
+    @Test
+    void listGamesFail() throws SQLException, DataAccessException {
+        // Drop the games table
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("DROP TABLE games")) {
+                preparedStatement.executeUpdate();
+            }
+        }
+
+        // Try to list games
+        assertThrows(DataAccessException.class, () -> {
+            gameDAO.listGames();
+        });
+    }
+
+    @Test
+    void updateGameSuccess() throws DataAccessException {
+        // Create game
+        gameDAO.createGame(new GameData(
+            0, null, null, "gameName", new ChessGame()
+        ));
+
+        // Join game
+        GameData expectedGameData = new GameData(
+            1, "whiteUsername", null, "gameName", new ChessGame()
+        );
+        gameDAO.updateGame(expectedGameData);
+
+        // Check if updated
+        assertEquals(expectedGameData, gameDAO.getGame(1));
+    }
+
+    @Test
+    void updateGameFail() throws DataAccessException {
+        // Create game
+        gameDAO.createGame(new GameData(
+            0, null, null, "gameName", new ChessGame()
+        ));
+
+        // Try to update with invalid game id
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            gameDAO.updateGame(new GameData(
+                2, "whiteUsername", null, null, new ChessGame()
+            ));
+        });
+        assertEquals("Error: bad request", exception.getMessage());
+    }
+
+    @Test
+    void clearGame() throws DataAccessException {
+        // Create game
+        int gameID = gameDAO.createGame(new GameData(
+            0, null, null, "gameName", new ChessGame()
+        ));
+
+        // Clear
+        gameDAO.clearGame();
+
+        // Check if game was cleared
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            gameDAO.getGame(gameID);
+        });
+        assertEquals("Error: bad request", exception.getMessage());
+    }
 }
