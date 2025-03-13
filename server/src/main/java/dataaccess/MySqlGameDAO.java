ca23f87b36cbdc84d98843ade2f26ddb52737ebe
apretado <156238182+apretado@users.noreply.github.com>
2025-03-12 22:59:01 -0600

    Implement MySqlGameDAO methods

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -1,8 +1,12 @@
 package dataaccess;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+
+import chess.ChessGame;
 import model.GameData;
 
 public class MySqlGameDAO implements GameDAO {
@@ -35,26 +39,109 @@ public class MySqlGameDAO implements GameDAO {
 
     @Override
     public int createGame(GameData game) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createGame'");
+        try (var conn = DatabaseManager.getConnection()) {
+            // Try to add user to database
+            try (var preparedStatement = conn.prepareStatement(
+                    "INSERT INTO games (game_name, game_state) VALUES (?, ?)",
+                    java.sql.Statement.RETURN_GENERATED_KEYS
+                )) {
+                preparedStatement.setString(1, game.gameName());
+                preparedStatement.setString(2, new Gson().toJson(game.game()));
+                preparedStatement.executeUpdate();
+
+                try (var rs = preparedStatement.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        // Return game id
+                        return rs.getInt(1);
+                    } else {
+                        throw new DataAccessException("Error getting game ID after inserting game into database");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            // Error code for "Column cannot be null"
+            if (e.getErrorCode() == 1048) { 
+                throw new DataAccessException("Error: game name cannot be null");
+            }
+            throw new DataAccessException("Error creating game: " + e.getMessage());
+        }
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getGame'");
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(
+                "SELECT game_id, white_username, black_username, game_name, game_state FROM games WHERE game_id = ?"
+            )) {
+                preparedStatement.setInt(1, gameID);
+                try (var rs = preparedStatement.executeQuery()) {
+                    if (rs.next()) {
+                        ChessGame chessGame = new Gson().fromJson(rs.getString("game_state"), ChessGame.class);
+                        return new GameData(
+                            rs.getInt("game_id"),
+                            rs.getString("white_username"),
+                            rs.getString("black_username"),
+                            rs.getString("game_name"),
+                            chessGame
+                        );
+                    } else {
+                        throw new DataAccessException("Error: bad request");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error getting game: " + e.getMessage());
+        }
     }
 
     @Override
     public List<GameData> listGames() throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listGames'");
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(
+                "SELECT game_id, white_username, black_username, game_name, game_state FROM games"
+            )) {
+                try (var rs = preparedStatement.executeQuery()) {
+                    List<GameData> gameList = new ArrayList<>();
+                    while (rs.next()) {
+                        ChessGame chessGame = new Gson().fromJson(rs.getString("game_state"), ChessGame.class);
+                        gameList.add(new GameData(
+                            rs.getInt("game_id"),
+                            rs.getString("white_username"),
+                            rs.getString("black_username"),
+                            rs.getString("game_name"),
+                            chessGame
+                        ));
+                    }
+                    return gameList;
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error listing games: " + e.getMessage());
+        }
     }
 
     @Override
     public void updateGame(GameData game) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateGame'");
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(
+                "UPDATE games SET white_username = ?, black_username = ?, game_name = ?, game_state = ? WHERE game_id = ?"
+            )) {
+                preparedStatement.setString(1, game.whiteUsername());
+                preparedStatement.setString(2, game.blackUsername());
+                preparedStatement.setString(3, game.gameName());
+                preparedStatement.setString(4, new Gson().toJson(game.game()));
+                preparedStatement.setInt(5, game.gameID());
+                
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                // Check if no game was updated
+                if (rowsAffected == 0) {
+                    throw new DataAccessException("Error: bad request");
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error updating game: " + e.getMessage());
+        }
     }
 
     @Override
