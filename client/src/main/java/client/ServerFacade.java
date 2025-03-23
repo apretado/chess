fa89e129b07d83219915d593ca14912144d32310
apretado <156238182+apretado@users.noreply.github.com>
2025-03-22 21:31:54 -0600

    Create ServerFacade shell class

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,14 @@
+package client;
+
+import model.AuthData;
+
+public class ServerFacade {
+    private int port;
+    public ServerFacade(int port) {
+        this.port = port;
+    }
+    public AuthData register(String string, String string2, String string3) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'register'");
+    }
+}
