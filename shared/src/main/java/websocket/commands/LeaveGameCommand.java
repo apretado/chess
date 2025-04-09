2097959355f3e3329ae327c95845ba1e76de77fb
apretado <156238182+apretado@users.noreply.github.com>
2025-04-08 19:50:38 -0600

    Create LeaveGameCommand class

---
+++ shared/src/main/java/websocket/commands/LeaveGameCommand.java
@@ -0,0 +1,9 @@
+package websocket.commands;
+
+public class LeaveGameCommand extends UserGameCommand {
+
+    public LeaveGameCommand(String authToken, Integer gameID) {
+        super(CommandType.LEAVE, authToken, gameID);
+    }
+
+}
